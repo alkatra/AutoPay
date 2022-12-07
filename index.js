506972feabd02281d88a94a2c54ec818f1ee843e
Sagar <sagargupta1827@gmail.com>
2022-12-07 19:27:35 +0545

    Add IP

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
   })
 );
 var cors = require("cors");
-app.use(cors({ credentials: true, origin: "https://alkatra.com" }));
+app.use(cors({ credentials: true, origin: "https://www.alkatra.com" }));
 const isAuth = require("./middleware/isAuth");
 const isNotAuth = require("./middleware/isNotAuth");
 var Client = require("./models/client");
